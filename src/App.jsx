8f8ef76e63bd7a7ad0aad2d958c676b159d969dc
amlanxbghn <amlanxconnect@gmail.com>
2024-07-26 15:06:22 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {  useState } from "react";
+import "./App.css";
 
+import { GoogleGenerativeAI } from "@google/generative-ai";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [loading, setLoading] = useState(false);
+  const [apiData, setApiData] = useState([]);
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [country, setCountry] = useState("");
+  const [hobbies, setHobbies] = useState("");
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyCy0Remq9c1uGAPvmIMsuTZZi_lG4EAy3k"
+  );
+  const fetchData = async () => {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = `Think you are a mom who has ${age} years old ${gender} in ${country}. your child's favourite things are ${hobbies}.You are a caring mother about your 
+    children and you have to tell a wonderful story for your child every single night. so generate a bedtime story for the chield bease on child's favourite things, country, age.
+    also end of the story you have to give some advice for kids to be good and do good for the sociaty. Also motivate them to belive themselefs`;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    setApiData(text);
+    setLoading(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(name, gender, age , country, hobbies);
+    fetchData();
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Google Gemini Pro AI Integration With React</h1>
+      <div className="mt-5 mb-5">
+        <form onSubmit={handleSubmit}>
+          <div className="row d-flex align-items-end">
+            <div className="col-lg-2">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="gender" className="form-label">
+                Gender
+              </label>
+              <select
+                className="form-select"
+                id="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="">Select Gender</option>
+                <option value="boy">Boy</option>
+                <option value="Girl">Girl</option>
+              </select>
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="age" className="form-label">
+                Age
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="country" className="form-label">
+                Country
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="hobbies" className="form-label">
+                Hobbies
+              </label>
+              <input
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="gender" className="form-label">
+                Gender
+              </label>
+              <select
+                className="form-select"
+                id="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="">Select Gender</option>
+                <option value="boy">Boy</option>
+                <option value="Girl">Girl</option>
+              </select>
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="age" className="form-label">
+                Age
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="country" className="form-label">
+                Country
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <label htmlFor="hobbies" className="form-label">
+                Hobbies
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="hobbies"
+                value={hobbies}
+                onChange={(e) => setHobbies(e.target.value)}
+              />
+            </div>
+            <div className="col-lg-2">
+              <button type="submit" className="btn btn-primary mt-3 col-lg-12">
+                Submit
+              </button>
+            </div>
+            
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="">
+        {!loading && <p className="text-align-left">{apiData}</p>}
+        {loading && <p>Loading...</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="mt-4">
+        Developed By <a href="https://amlan.tech">AMLAN</a>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
